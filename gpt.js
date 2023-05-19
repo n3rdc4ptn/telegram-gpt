b1f234d6060e2dff5976545ef104e98b41d11c25
Moritz Reich <moritz.reich@minest.de>
2023-05-19 09:36:10 +0200

    changed to latest

--- gpt.js
+++ gpt.js
@@ -12,13 +12,12 @@ async function answerChat(user_id, messages) {
     const client = await getOpenAI();
 
     const response = await client.createChatCompletion({
-        model: 'gpt-4',
+        model: 'gpt-3.5-turbo',
         messages,
         user: user_id
     });
 
     console.log('User: ', user_id);
-    console.log('Msg: ', messages.slice(-1)[0].content);
     console.log(`Usage:`, response.data.usage);
 
     return response.data.choices[0].message?.content;
